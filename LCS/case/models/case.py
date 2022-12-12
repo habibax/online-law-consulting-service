ff191ce38aa85387163c444c41c7c15dc8617616
MSHARIF <m.sharif@megatoken.net>
2022-12-12 18:19:12 +0330

    case model added to project

---
+++ LCS/case/models/case.py
@@ -0,0 +1,26 @@
+from datetime import timedelta
+from django.db import models
+from django.utils.timezone import now
+
+from common.common_models import BaseModel
+
+
+class Stake(BaseModel):
+    class State(models.TextChoices):
+        PENDING_ADMIN = ("pending_admin",)
+        PENDING_LAWYER = ("pending_lawyer",)
+        ACCEPTED = ("accepted",)
+        REJECTED = ("rejected",)
+
+    state = models.CharField(
+        max_length=20, default=State.PENDING_ADMIN, null=False, choices=State.choices
+    )
+    user = models.ForeignKey("acc.User", on_delete=models.CASCADE, related_name="case")
+    lawyer = models.ForeignKey("acc.User", on_delete=models.CASCADE, related_name="case") 
+
+    title = models.CharField(max_length=225, null=False)
+    content = models.TextField()  
+    issue_date = models.DateField(null=False, default=now)
+
+    
+
